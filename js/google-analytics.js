7517360a5876f9e76f574d39f65e1048e49be77c
Xu Haidong <xu.haidong@outlook.com>
2019-05-01 20:16:31 +0800

    Site updated: 2019-05-01 20:16:31

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,1 +1,14 @@
-!function(e,a,t,n,g,c,o){e.GoogleAnalyticsObject=g,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=1*new Date,c=a.createElement(t),o=a.getElementsByTagName(t)[0],c.async=1,c.src="https://www.google-analytics.com/analytics.js",o.parentNode.insertBefore(c,o)}(window,document,"script",0,"ga"),ga("create","UA-73442912-1","auto"),ga("send","pageview");
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-73442912-1', 'auto');
+ga('send', 'pageview');
--- js/jquery.appear.js
+++ js/jquery.appear.js
@@ -1,1 +1,101 @@
-!function(f){var i,a=[],n=!1,o=!1,p={interval:250,force_process:!1},s=f(window);function c(){o=!1;for(var e=0;e<a.length;e++){var r=f(a[e]).filter(function(){return f(this).is(":appeared")});if(r.trigger("appear",[r]),i){var t=i.not(r);t.trigger("disappear",[t])}i=r}}f.expr[":"].appeared=function(e){var r=f(e);if(!r.is(":visible"))return!1;var t=s.scrollLeft(),i=s.scrollTop(),a=r.offset(),n=a.left,o=a.top;return o+r.height()>=i&&o-(r.data("appear-top-offset")||0)<=i+s.height()&&n+r.width()>=t&&n-(r.data("appear-left-offset")||0)<=t+s.width()},f.fn.extend({appear:function(e){var r=f.extend({},p,e||{}),t=this.selector||this;if(!n){var i=function(){o||(o=!0,setTimeout(c,r.interval))};f(window).scroll(i).resize(i),n=!0}return r.force_process&&setTimeout(c,r.interval),a.push(t),f(t)}}),f.extend({force_appear:function(){return!!n&&(c(),!0)}})}(jQuery);
+/*
+ * jQuery appear plugin
+ *
+ * Copyright (c) 2012 Andrey Sidorov
+ * licensed under MIT license.
+ *
+ * https://github.com/morr/jquery.appear/
+ *
+ * Version: 0.3.3
+ */
+(function($) {
+  var selectors = [];
+
+  var check_binded = false;
+  var check_lock = false;
+  var defaults = {
+    interval: 250,
+    force_process: false
+  }
+  var $window = $(window);
+
+  var $prior_appeared;
+
+  function process() {
+    check_lock = false;
+    for (var index = 0; index < selectors.length; index++) {
+      var $appeared = $(selectors[index]).filter(function() {
+        return $(this).is(':appeared');
+      });
+
+      $appeared.trigger('appear', [$appeared]);
+
+      if ($prior_appeared) {
+        var $disappeared = $prior_appeared.not($appeared);
+        $disappeared.trigger('disappear', [$disappeared]);
+      }
+      $prior_appeared = $appeared;
+    }
+  }
+
+  // "appeared" custom filter
+  $.expr[':']['appeared'] = function(element) {
+    var $element = $(element);
+    if (!$element.is(':visible')) {
+      return false;
+    }
+
+    var window_left = $window.scrollLeft();
+    var window_top = $window.scrollTop();
+    var offset = $element.offset();
+    var left = offset.left;
+    var top = offset.top;
+
+    if (top + $element.height() >= window_top &&
+        top - ($element.data('appear-top-offset') || 0) <= window_top + $window.height() &&
+        left + $element.width() >= window_left &&
+        left - ($element.data('appear-left-offset') || 0) <= window_left + $window.width()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  $.fn.extend({
+    // watching for element's appearance in browser viewport
+    appear: function(options) {
+      var opts = $.extend({}, defaults, options || {});
+      var selector = this.selector || this;
+      if (!check_binded) {
+        var on_check = function() {
+          if (check_lock) {
+            return;
+          }
+          check_lock = true;
+
+          setTimeout(process, opts.interval);
+        };
+
+        $(window).scroll(on_check).resize(on_check);
+        check_binded = true;
+      }
+
+      if (opts.force_process) {
+        setTimeout(process, opts.interval);
+      }
+      selectors.push(selector);
+      return $(selector);
+    }
+  });
+
+  $.extend({
+    // force elements's appearance check
+    force_appear: function() {
+      if (check_binded) {
+        process();
+        return true;
+      };
+      return false;
+    }
+  });
+})(jQuery);
